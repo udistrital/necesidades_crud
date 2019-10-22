6c905e06f47170aa46de459ba565656348e4df13
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2019-10-22 10:31:05 -0500

    se corrige el model de producto rubro, añadiendo campo monto parcial

--- database/migrations/20191015_111055_crear_tabla_producto_rubro_necesidad.go
+++ database/migrations/20191015_111055_crear_tabla_producto_rubro_necesidad.go
@@ -20,7 +20,7 @@ func init() {
 // Run the migrations
 func (m *CrearTablaProductoRubroNecesidad_20191015_111055) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	m.SQL("CREATE TABLE necesidades.producto_rubro_necesidad (id serial NOT NULL,rubro_necesidad_id integer NOT NULL,producto_id varchar NOT NULL,activo boolean NOT NULL DEFAULT true,fecha_creacion timestamp NOT NULL,fecha_modificacion timestamp NOT NULL,CONSTRAINT pk_producto_rubro_necesidad PRIMARY KEY (id));")
+	m.SQL("CREATE TABLE necesidades.producto_rubro_necesidad (id serial NOT NULL,rubro_necesidad_id integer NOT NULL,producto_id varchar NOT NULL,monto_parcial numeric(20,7) NOT NULL,activo boolean NOT NULL DEFAULT true,fecha_creacion timestamp NOT NULL,fecha_modificacion timestamp NOT NULL,CONSTRAINT pk_producto_rubro_necesidad PRIMARY KEY (id));")
 	m.SQL("ALTER TABLE necesidades.producto_rubro_necesidad OWNER TO desarrollooas;")
 
 }
