efe320dbef3b396ebafe4445b89820836871ec98
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2019-10-24 11:17:52 -0500

    ajuste modelo

--- models/fuente_actividad.go
+++ models/fuente_actividad.go
@@ -13,7 +13,7 @@ import (
 type FuenteActividad struct {
 	Id                       int            `orm:"column(id);pk;auto"`
 	FuenteId                 string         `orm:"column(fuente_id)"`
-	ActividadMetaNecesidadId *ActividadMeta `orm:"column(meta_rubro_necesidad_id);rel(fk)"`
+	ActividadMetaNecesidadId *ActividadMeta `orm:"column(actividad_meta_necesidad_id);rel(fk)"`
 	MontoParcial             float64        `orm:"column(monto_parcial)"`
 	Activo                   bool           `orm:"column(activo)"`
 	FechaCreacion            time.Time      `orm:"auto_now_add;column(fecha_creacion);type(timestamp without time zone)"`
