22958d2267cee3e5d23e705b4d3bdc524ee91c58
CarlosGutierrezArdila <chgutierreza@correo.udistrital.edu.co>
2019-10-27 21:40:54 -0500

    ajuste variable database en main para funcionamiento en local
    se corrige el llamado a una variable generada por beego que ya no existe en el app conf y se reemplaza por la url basada en variables de entorno

--- main.go
+++ main.go
@@ -35,7 +35,7 @@ func init() {
 
 func main() {
 	orm.Debug = true
-	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	orm.RegisterDataBase("default", "postgres", "postgres://"+beego.AppConfig.String("PGuser")+":"+beego.AppConfig.String("PGpass")+"@"+beego.AppConfig.String("PGurls")+"/"+beego.AppConfig.String("PGdb")+"?sslmode=disable&search_path="+beego.AppConfig.String("PGschemas")+"")
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
 		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
