c63acc72dcfcd48bb858eaed1686eae4b304fea5
DanKazuky <daguazacot@ucentral.edu.co>
2022-02-09 23:37:35 -0500

    feat: correcion migration

--- database/migrations/20220208_232314_necesidad.go
+++ database/migrations/20220208_232314_necesidad.go
@@ -20,13 +20,13 @@ func init() {
 // Run the migrations
 func (m *Necesidad_20220208_232314) Up() {
 	// use m.SQL("CREATE TABLE ...") to make schema update
-	m.SQL("EXEC sp_rename 'necesidades.necesidad.consecutivo_solicitud', 'consecutivo', 'COLUMN';")
+	m.SQL("ALTER TABLE necesidades.necesidad RENAME COLUMN consecutivo_solicitud TO consecutivo;")
 	m.SQL("ALTER TABLE necesidades.necesidad DROP COLUMN consecutivo_necesidad;")
 }
 
 // Reverse the migrations
 func (m *Necesidad_20220208_232314) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
-	m.SQL("EXEC sp_rename 'necesidades.necesidad.consecutivo', 'consecutivo_solicitud', 'COLUMN';")
+	m.SQL("ALTER TABLE necesidades.necesidad RENAME COLUMN consecutivo TO consecutivo_solicitud;")
 	m.SQL("ALTER TABLE necesidades.necesidad ADD consecutivo_necesidad int4 NULL;")
 }
